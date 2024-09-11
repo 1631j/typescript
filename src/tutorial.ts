5dfe4dad2f599693586ae88a2188cea222bb50e6
1631j <73850091+1631j@users.noreply.github.com>
2024-09-11 16:40:50 +0600

    Utility Types

--- src/tutorial.ts
+++ src/tutorial.ts
@@ -504,6 +504,7 @@ for (const revenue in monthlyIncomes) {
 }
 */
 
+/*
 ///////////////////////////////////////////////
 //// GENERICS ////////
 const echo = <T>(arg: T): T => arg;
@@ -545,3 +546,125 @@ store.state = "Jane";
 const myState = new StateObject<(string | number | boolean)[]>([15]);
 myState.state = [true, "hello", 15];
 console.log(myState.state);
+*/
+
+///////////////////////////////////////////////
+//// UTILITY TYPES ////////
+
+//// Partial ////
+
+interface Assignment {
+  studentId: string;
+  title: string;
+  grade: number;
+  verified?: boolean;
+}
+
+const updateAssignment = (
+  assign: Assignment,
+  propsToUpdate: Partial<Assignment>
+): Assignment => {
+  return { ...assign, ...propsToUpdate };
+};
+
+const assign1: Assignment = {
+  studentId: "compsci123",
+  title: "Final Project",
+  grade: 0,
+};
+
+console.log(updateAssignment(assign1, { grade: 100 }));
+const assignGraded: Assignment = updateAssignment(assign1, { grade: 100 });
+// console.log(assignGraded);
+
+//// Required  and READONLY////
+
+const recordAssignment = (assign: Required<Assignment>): Assignment => {
+  // send to database, etc.
+  return assign;
+};
+
+const assignVerified: Readonly<Assignment> = {
+  ...assignGraded,
+  verified: true,
+};
+
+// assignVerified(assignGraded)
+recordAssignment({ ...assignGraded, verified: true });
+
+// Record
+
+const hexColorMap: Record<string, string> = {
+  red: "#ff0000",
+  green: "#00ff00",
+  blue: "#0000ff",
+};
+
+type Students = "john" | "jane" | "james";
+type LetterGrades = "A" | "B" | "C" | "D" | "U";
+
+const finalGrades: Record<Students, LetterGrades> = {
+  john: "A",
+  jane: "B",
+  james: "U",
+};
+
+interface Grades {
+  assign1: number;
+  assign2: number;
+}
+
+const gradeData: Record<Students, Grades> = {
+  john: { assign1: 100, assign2: 90 },
+  jane: { assign1: 90, assign2: 100 },
+  james: { assign1: 0, assign2: 0 },
+};
+
+// Pick and Omit
+
+type AssignResult = Pick<Assignment, "studentId" | "grade">;
+
+const score: AssignResult = {
+  studentId: "k123",
+  grade: 80,
+};
+
+type AssignPreview = Omit<Assignment, "grade" | "verified">;
+
+const preview: AssignPreview = {
+  studentId: "k123",
+  title: "Final Project",
+};
+
+// Exclude and Extract
+
+type adjustedGrade = Exclude<LetterGrades, "U">;
+
+type highGrades = Extract<LetterGrades, "A" | "B">;
+
+// NonNullable
+
+type AllPossibleGrade = "Dave" | "James" | null | undefined;
+type NamesOnly = NonNullable<AllPossibleGrade>;
+
+// ReturnType
+
+// type newAssign = {title: string, points: number}
+
+const createNewAssign = (title: string, points: number) => {
+  return { title, points };
+};
+
+type newAssign = ReturnType<typeof createNewAssign>;
+
+const tsAssign: newAssign = createNewAssign("Utility Types", 100);
+console.log(tsAssign);
+
+// parameters
+
+type AssignParams = Parameters<typeof createNewAssign>;
+
+const assignArgs: AssignParams = ["Utility Types", 100];
+
+const tsAssign2: newAssign = createNewAssign(...assignArgs);
+console.log(tsAssign2);

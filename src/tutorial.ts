c2560d42840653a48fae04e76265ef4c2cdf7dfb
1631j <73850091+1631j@users.noreply.github.com>
2024-09-11 07:36:06 +0600

    functions

--- src/tutorial.ts
+++ src/tutorial.ts
@@ -30,3 +30,22 @@ const someValue = result;
 
 // run time error
 someValue.myMethod();
+
+/*
+Create a new array of names.
+Write a new function that checks if a name is in your array. This function should take a name as a parameter and return a boolean.
+Use this function to check if various names are in your array and log the results.
+
+const names = ["john", "peter", "susan", "anna"];
+
+function isNameInList(name: string): boolean {
+  return names.includes(name);
+}
+
+let nameToCheck: string = 'Jane';
+if (isNameInList(nameToCheck)) {
+  console.log(`${nameToCheck} is in the list.`);
+} else {
+  console.log(`${nameToCheck} is not in the list.`);
+}
+  */

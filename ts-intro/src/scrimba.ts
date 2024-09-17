da21ad2aa2450d8a7f554cd755ac24b576c7b58b
1631j <73850091+1631j@users.noreply.github.com>
2024-09-17 08:38:05 +0300

    Refreshing TypeScript

---
+++ ts-intro/src/scrimba.ts
@@ -0,0 +1,23 @@
+// let definetlyNotAString: any = "I am a string";
+
+// let strLength = (<string>definetlyNotAString ).length;
+
+// /////// types with arrays
+// const example1: string[] = ["hello", "world"];
+
+// const exaplmple2: (number | boolean)[] = [1, 2, 3, 4, true];
+
+// const example3: boolean[][] = [[true, false, true]];
+
+// /////// Tuples
+
+// const exampleTuple: [string, number] = ["hello", 10];
+
+//// Enums
+
+////// Objects
+// const example1: object = undefined;
+
+// const example2: Object = NaN;
+
+// const example3: {} = {};
